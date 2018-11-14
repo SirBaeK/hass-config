cca186500820c42116bf233e2bce80796a711227
Jakub Kafka <kafkic@gmail.com>
2018-11-14 18:04:30 +0100

    Update backuplog.sh
    Addes some comment

--- backuplog.sh
+++ backuplog.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 logfile="home-assistant"
 number=0
-
+#in homeassistant folder check for log
 while test -e "/home/homeassistant/.homeassistant/$logfile$suffix.log"; do
     (( ++number ))
     suffix="$( printf -- '-%02d' "$number" )"
 done
-
+#if is there, rename it to home-assistat-<increasing numbers>.log . actually doing backup log file
+#TODO: make from multiple log one log
 fname="$logfile$suffix.log"
 
 printf 'Will use "%s" as filename\n' "$fname"
