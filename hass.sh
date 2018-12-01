c7d2029109150895d29d7ee4b40a18b6f18fe609
Jakub Kafka <kafkic@gmail.com>
2018-12-01 21:58:54 +0100

    Add files via upload

--- hass.sh
+++ hass.sh
@@ -2,9 +2,18 @@
 #Made by SirBaeK 11/2018
 
 HASSPATH="/opt/homeassistant"
-HASSUSER="homeassistant"
+HASSUSER="kafkicz"
+PID_FILE="/var/run/hass.pid"
+PID="999999"
+
+
 sudo ./backuplog.sh
 
+hasspid() {
+    echo "making hass.pid"
+    sudo bash -c "echo $PID > $PID_FILE"
+    sudo chown $HASSUSER $PID_FILE
+}
 
 
 case "$1" in
@@ -44,16 +53,56 @@ case "$1" in
 	    echo "pip modules updated"
 	    ;;
   -s|start) #start home assistant from python virtual environment in /opt/homeassistant: my hardlinks, MUST BE EDITED
-	    echo "Spoustim java virtual enviroment + hass"
-	    sudo -u $HASSUSER -H sh -c "python3.6 -m venv $HASSPATH; echo 'venv set'; . $HASSPATH/bin/activate; echo 'venv activated'; $HASSPATH/bin/hass --open-ui --daemon -c /home/homeassistant/.homeassistant/ -v; echo 'hass started'"
+            if [ -f $PID_FILE ] && kill -0 $(cat $PID_FILE) 2> /dev/null; then
+              echo 'Service already running'           
+            else
+	      echo "Spoustim hass"
+            hasspid
+	    #python3.6 -m venv $HASSPATH; echo 'venv set'; . $HASSPATH/bin/activate; echo 'venv activated';
+	    sudo -u $HASSUSER -H sh -c "$HASSPATH/bin/hass --daemon -c /home/kafkicz/.homeassistant/ --verbose --open-ui --pid-file $PID_FILE; echo 'hass started'"
 	    sleep 5
 	    sudo -u kafkicz -H sh -c "firefox -P default -new-window http://192.168.11.7:8123"
+            fi
 	    ;;
+  -S|Stop)
+            if [ ! -f "$PID_FILE" ] || ! kill -0 $(cat "$PID_FILE") 2> /dev/null; then
+              echo 'Service not running'
+            else
+            	sudo kill -9 $PID
+                echo "$PID - $PID_FILE killed"
+            fi
+            ;;
+  -A|Autostart)
+            sudo bash -c 'cat <<EOT >>/etc/systemd/system/home-assistant@kafkicz.service
+[Unit]
+Description=Home Assistant
+After=network-online.target
+
+[Service]
+Type=simple
+User=%i
+ExecStart=/opt/homeassistant/bin/hass -c /home/kafkicz/.homeassistant/ --verbose --open-ui
+
+[Install]
+WantedBy=multi-user.target
+EOT
+'
+            sudo systemctl --system daemon-reload
+            sudo systemctl enable home-assistant@kafkicz
+            sudo systemctl start home-assistant@kafkicz
+            ;;
 esac
 
 
 
 echo "konec"
+
+
+
+
+
+
+
 
 #################################################################################################
 #					basicaly my steps					#
@@ -74,36 +123,36 @@ echo "konec"
 #  sudo mkdir $HASSPATH;
 #  sudo chown $HASSUSER:$HASSUSER $HASSPATH/
 #fi
-
-
+#
+#
 #python3.6 -m venv $HASSPATH
-
-
+#
+#
 #if [ "$*" == "--python" ]; then 
 #  sudo add-apt-repository -y ppa:deadsnakes/ppa;
 #  sudo apt-get update;
 #  sudo apt-get install -y python3.6-venv python3.6-dev
 #fi
-
-
+#
+#
 #if [ "$*" == "--adduser" ]; then 
 #  sudo useradd $HASSUSER -m -U -p $HASSUSER
 #fi
-
+#
 #if [ "$*" == "--upgrade" ]; then 
 #  cd $HASSPATH;
 #  source $HASSPATH/bin/activate;
 #  python3.6 -m pip install --upgrade homeassistant
 #fi
-
-
+#
+#
 #sudo -u $HASSUSER -H -s
-
+#
 #python3.6 -m venv $HASSPATH
 #. $HASSPATH/bin/activate
 #python3.6 -m pip install wheel
 #python3.6 -m pip install homeassistant
-
+#
 #pip3 install home-assistant-frontend sqlalchemy aiohttp_cors PyQRCode pyotp warrant mutagen xmltodict netdisco dis#tro
 #$HASSPATH/bin/hass --open-ui --daemon
 #sudo -u kafkicz -H -s
